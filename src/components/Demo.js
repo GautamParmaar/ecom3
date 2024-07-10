968d3c28a4400e982764e1f163b5a797246a5c25
Gautam Parmar <112480280+GautamParmaar@users.noreply.github.com>
2024-07-10 15:15:19 +0530

    new home page completed

--- src/components/Demo.js
+++ src/components/Demo.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import Header from './Header'
 import Features from './Features'
+import About from './About'
+import Testimonial from './Testimonial'
+import Navbar2 from './Navbar'
 
 function Demo() {
   return (
     <>
+    <Navbar2/>
     <Header/>
     <Features/>
+    <About/>
+    <Testimonial/>
     </>
   )
 }
